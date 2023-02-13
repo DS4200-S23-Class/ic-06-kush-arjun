fa148285951a1d9fcbc126b41444d745c3587da0
arjunkarkare <91080637+arjunkarkare@users.noreply.github.com>
2023-02-13 16:32:08 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function changeColor() {
+        let line = getElementById('frame');
+        let color = line.color;
+
+        if (color == 'darkblue') {
+                let color = 'red';
+        }
+
+}
+
+document.getElementById("subButton")
+        .addEventListener('click',submitClicked);
